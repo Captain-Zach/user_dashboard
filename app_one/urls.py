97e4d9739a1be09a0c8b65f524d363d644be7621
Captain_Zach <zacharytjones1@gmail.com>
2020-01-20 16:00:50 -0800

    urls and paths done to limits

--- app_one/urls.py
+++ app_one/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('signin', views.signin),
+    path('register', views.register),
+    path('dashboard', views.dashboard),
+    path('dashboard/admin', views.admin_dashboard),
+    path('users/new', views.users_new),
+    path('users/show/<int:user_id>', views.the_wall),
+    path('users/edit/<int:user_id>', views.edit_user),
+    path('users/edit', views.edit_self),
+    # POST METHODS
+    path('log_in', views.log_in),
+    path('log_out', views.log_out),
+    path('create_user', views.create_user),
     
 ]
--- app_one/views.py
+++ app_one/views.py
@@ -7,7 +7,39 @@ import time
 
 # Create your views here.
 def index(request):
-    
-
-
     return render(request, 'index.html')
+
+def signin(request):
+    return render(request, "sign_in.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def dashboard(request):
+    return render(request, "user_dashboard.html")
+
+def admin_dashboard(request):
+    return render(request, "admin_dashboard.html")
+
+def users_new(request):
+    return render(request, "add_user.html")
+
+def the_wall(request):
+    return render(request, "wall.html")
+
+def edit_user(request):
+    return render(request, "admin_edit.html")
+
+def edit_self(request):
+    return render(request, "user_edit.html")
+
+def log_in(request):
+    return redirect('/dashboard')
+
+def log_out(request):
+    return redirect('/')
+
+def create_user(request):
+    #create user here, same route
+    #pass in linkback
+    return redirect('/dashboard')
